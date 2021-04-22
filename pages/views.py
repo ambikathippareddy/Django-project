c249e7ff72cc81ef776577045753632a89d42277
ambikathippareddy <ambika.thipareddy@gmail.com>
2021-04-22 21:42:42 +0530

    first commit

---
+++ pages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'pages/home.html')
+
+def about(request):
+    return render(request,'pages/about.html')
+    
+def Services(request):
+    return render(request,'pages/Services.html')
+
+def contact(request):
+    return render(request,'pages/contact.html')
